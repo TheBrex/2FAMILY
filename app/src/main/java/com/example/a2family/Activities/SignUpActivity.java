f994c56f01fdb0b14d1ca7e31fe7c9c2ca9a18c7
TheBrex <marcobresciani.1998@gmail.com>
2022-01-11 15:46:39 +0100

    Remove hard coded Strings and translate

--- app/src/main/java/com/example/a2family/Activities/SignUpActivity.java
+++ app/src/main/java/com/example/a2family/Activities/SignUpActivity.java
@@ -1,8 +1,5 @@
 package com.example.a2family.Activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -12,15 +9,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
 import com.example.a2family.Classes.User;
 import com.example.a2family.Interfaces.HelperInterface;
 import com.example.a2family.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 
 public class SignUpActivity extends BaseActivity implements HelperInterface {
